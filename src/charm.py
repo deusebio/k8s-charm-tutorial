b4445f59b88a2604f7640a901c993b9cf599123c
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-11-10 13:38:00 +0100

    delete config file clean up config changed function
    updated charm name, description
    updated OCI image
    changed pebble layer
    renamed class

--- src/charm.py
+++ src/charm.py
@@ -17,22 +17,22 @@ import logging
 from ops.charm import CharmBase
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.pebble import Layer
 
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
-VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
 
-
-class FastapiDemoCharm(CharmBase):
+class FastAPIDemoCharm(CharmBase):
     """Charm the service."""
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+        self.pebble_service_name = "fastapi-service"
+        self.framework.observe(self.on.demo_server_pebble_ready, self._on_demo_server_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
 
-    def _on_httpbin_pebble_ready(self, event):
+    def _on_demo_server_pebble_ready(self, event):
         """Define and start a workload using the Pebble API.
 
         Change this example to suit your needs. You'll need to specify the right entrypoint and
@@ -43,7 +43,7 @@ class FastapiDemoCharm(CharmBase):
         # Get a reference the container attribute on the PebbleReadyEvent
         container = event.workload
         # Add initial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", self._pebble_layer, combine=True)
+        container.add_layer("fastapi_demo", self._pebble_layer, combine=True)
         # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
         container.replan()
         # Learn more about statuses in the SDK docs:
@@ -51,55 +51,33 @@ class FastapiDemoCharm(CharmBase):
         self.unit.status = ActiveStatus()
 
     def _on_config_changed(self, event):
-        """Handle changed configuration.
-
-        Change this example to suit your needs. If you don't need to handle config, you can remove
-        this method.
-
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        # Fetch the new config value
-        log_level = self.model.config["log-level"].lower()
-
-        # Do some validation of the configuration option
-        if log_level in VALID_LOG_LEVELS:
-            # The config is good, so update the configuration of the workload
-            container = self.unit.get_container("httpbin")
-            # Verify that we can connect to the Pebble API in the workload container
-            if container.can_connect():
-                # Push an updated layer with the new config
-                container.add_layer("httpbin", self._pebble_layer, combine=True)
-                container.replan()
-
-                logger.debug("Log level for gunicorn changed to '%s'", log_level)
-                self.unit.status = ActiveStatus()
-            else:
-                # We were unable to connect to the Pebble API, so we defer this event
-                event.defer()
-                self.unit.status = WaitingStatus("waiting for Pebble API")
-        else:
-            # In this case, the config option is bad, so block the charm and notify the operator.
-            self.unit.status = BlockedStatus("invalid log level: '{log_level}'")
+        return
 
     @property
     def _pebble_layer(self):
         """Return a dictionary representing a Pebble layer."""
-        return {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+        command = " ".join(
+            [
+                "uvicorn",
+                "api_demo_server.app:app",
+                "--host=0.0.0.0",
+                f"--port=8000",
+            ]
+        )
+        pebble_layer = {
+            "summary": "FastAPI demo service",
+            "description": "pebble config layer for FastAPI demo server",
             "services": {
-                "httpbin": {
+                self.pebble_service_name: {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "fastapi demo",
+                    "command": command,
                     "startup": "enabled",
-                    "environment": {
-                        "GUNICORN_CMD_ARGS": f"--log-level {self.model.config['log-level']}"
-                    },
                 }
             },
         }
+        return Layer(pebble_layer)
 
 
 if __name__ == "__main__":  # pragma: nocover
-    main(FastapiDemoCharm)
+    main(FastAPIDemoCharm)
